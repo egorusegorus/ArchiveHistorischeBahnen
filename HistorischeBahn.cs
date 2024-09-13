9b7b6568853dd7a3a8e48d84397560c5e4dd2a2e
egorusegorus <tomaszokraszewski@gmail.com>
2024-09-12 22:37:24 -0700

    Add files via upload

---
+++ HistorischeBahn.cs
@@ -0,0 +1,30 @@
+﻿namespace ArchiveHistorischeBahnen
+{
+    [Serializable]
+    public class HistorischeBahn
+    {
+        public string? BetriebArt;
+        public DateTime WannWarProduziert;
+        public string? WoWarProduziert;
+        public string? BisWannInBetrieb;
+        public int Alter;
+
+        public HistorischeBahn(){}
+        public HistorischeBahn(DateTime wannWarProduziert)
+{
+    WannWarProduziert = wannWarProduziert;
+
+    DateTime heute = DateTime.Now;
+    int h1 = heute.Year;
+    int h2 = wannWarProduziert.Year;
+
+    Alter = h1 - h2;
+}
+
+        public override string ToString() { return $"{BetriebArt}{WannWarProduziert.ToString()}{WoWarProduziert}{BisWannInBetrieb}{Alter.ToString()}"; }
+
+
+    }
+
+    }
+
